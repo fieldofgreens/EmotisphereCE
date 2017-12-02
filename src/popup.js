0a424ac1cc4130f660a32a172bc32620a425e46d
Phillip Jun <29644671+waffiltime@users.noreply.github.com>
2017-12-02 15:21:14 -0800

    POST request working with preventDefault on submit button

--- src/popup.js
+++ src/popup.js
@@ -1,54 +1,64 @@
 $(document).ready(function() {
+  
+  let app = {
+  
+    server: 'https://emotisphere2.herokuapp.com/entries',
+  
+    send: function(data) {
+      console.log('data in send =', data);
+      return $.ajax({
+        type: 'POST',
+        url: app.server,
+        crossDomain: true,
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(data)
+      });
+    },
 
-let app = {
+    handleSubmit: function(thing) {
+      let data = {
+        text: thing.value,
+        title: "Guest Post from Extension"
+      };
+      console.log('data in handlesubmit =', data);
 
-  // server: 'https://emotisphere2.herokuapp.com/entries',
+      app.send(data).then((result)=>{
+        console.log('send then success result =', result);
+      }).catch((error)=>{
+        console.log('catch error =', error);
+      });
+      // .then().catch();
+      // .then(function() {
+      //   console.log('post then?');
+      // });
 
-  // send: function(message) {
-  //   $.ajax({
-  //     type: 'POST',
-  //     url: app.server,
-  //     data: {
-  //       // title: this.state.newestTitle,
-  //       text: message,
-  //       // username: this.state.username
-  //     },
-  //     success: function() {
-  //       console.log('post success')
-  //     }
-  //   }).then(function() {
-  //     context.props.rerender();
-  //   }); 
-  // },
-
-  handleSubmit: function(thing) {
-    console.log('thing ', thing.value);
-    console.log(document.getElementById('textfield').value);
-    let message = {
-      text: thing.value
-    };
-    // console.log(message.text);
-    // app.send(message);
-    // event.prevent Default();
-  },
-
-  init: function() {
-    console.log('initialized');
-    // app.$message = document.getElementById('textfield');
-
-    document.getElementById('submitbutton').addEventListener('click', function() {
-      app.handleSubmit(document.getElementById('textfield'));
-    });
-     //try document.getElementById
-    // app.$send = $('#submit');
-    // app.$submit.submit(function() {
-    //   console.log('submit?');
-    // });
-    // app.$send.on('click', app.handleSubmit);
+      // console.log('thing ', thing.value);
+      // console.log(document.getElementById('textfield').value);
+      // console.log('message = ', message);
+      // console.log('message.text = ', message.text);
+      // console.log(message.text);
+      // event.prevent Default();
+    },
+  
+    init: function() {
+      console.log('initialized');
+      document.getElementById('submitbutton').addEventListener('click', function(event) {
+        event.preventDefault();
+        app.handleSubmit(document.getElementById('textfield'));
+      });
+      // app.$message = document.getElementById('textfield');
+      //try document.getElementById
+      // app.$send = $('#submit');
+      // app.$submit.submit(function() {
+      //   console.log('submit?');
+      // });
+      // app.$send.on('click', app.handleSubmit);
+    }
   }
-}
-
-app.init();
-
-});
-
+  
+  app.init();
+  
+  });
+  
+  
