77b9e71b4e451eadc3ce4e4a8ee4840d862c38ea
Phillip Jun <29644671+waffiltime@users.noreply.github.com>
2017-12-01 23:29:24 -0800

    WIP submit from html to jquery

---
+++ src/popup.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+let app = {
+
+  server: 'https://emotisphere2.herokuapp.com/entries',
+
+  send: function(message) {
+    $.ajax({
+      type: 'POST',
+      url: app.server,
+      data: {
+        // title: this.state.newestTitle,
+        text: message,
+        // username: this.state.username
+      },
+      success: function() {
+        console.log('post success')
+      }
+    }).then(function() {
+      context.props.rerender();
+    }); 
+  },
+
+  handleSubmit: function(thing) {
+    console.log('thing ', thing);
+   console.log(document.getElementById('textfield'));
+    // let message = {
+    //   text: app.$message.val()
+    // };
+    // console.log(message.text);
+    // app.send(message);
+    // event.preventDefault();
+  },
+
+  init: function() {
+    console.log('initialized');
+    // app.$message = document.getElementById('textfield');
+
+    document.getElementById('submitbutton').addEventListener('click', function() {
+      app.handleSubmit(document.getElementById('textfield'));
+    });
+     //try document.getElementById
+    // app.$send = $('#submit');
+    // app.$submit.submit(function() {
+    //   console.log('submit?');
+    // });
+    // app.$send.on('click', app.handleSubmit);
+  }
+}
+
+app.init();
+
+});
+
