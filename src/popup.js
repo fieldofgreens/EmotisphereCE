297c87f9ef7c8c69e32a4792d64a39ffe33356da
Phillip Jun <29644671+waffiltime@users.noreply.github.com>
2017-12-03 18:48:55 -0800

    make chart render on click

--- src/popup.js
+++ src/popup.js
@@ -27,8 +27,55 @@
       app.send(data).then((result)=>{
         let resultsTitle = document.getElementById('resultsTitle');
         let resultsDiv = document.getElementById('results');
+        let chartContainer = document.getElementById('chartContainer');
         resultsDiv.append('Please see your results below ');
         resultsDiv.append(JSON.stringify(result));
+        let chart = function() {
+          Highcharts.chart('chartContainer', {
+            chart: {
+              polar: true,
+              type: 'line'
+            },        
+            title: {
+              text: 'Budget vs spending',
+              x: -80
+            },        
+            pane: {
+              size: '80%'
+            },        
+            xAxis: {
+              categories: ['Sales', 'Marketing', 'Development', 'Customer Support', 'Information Technology', 'Administration'],
+              tickmarkPlacement: 'on',
+              lineWidth: 0
+            },        
+            yAxis: {
+              gridLineInterpolation: 'polygon',
+              lineWidth: 0,
+              min: 0
+            },        
+            tooltip: {
+              shared: true,
+              pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
+            },        
+            legend: {
+              align: 'right',
+              verticalAlign: 'top',
+              y: 70,
+              layout: 'vertical'
+            },
+            series: [{
+              name: 'Allocated Budget',
+              data: [43000, 19000, 60000, 35000, 17000, 10000],
+              pointPlacement: 'on'
+            }, {
+              name: 'Actual Spending',
+              data: [50000, 39000, 42000, 31000, 26000, 14000],
+              pointPlacement: 'on'
+            }]
+          });
+        };
+                
+        chartContainer.append(chart());
         console.log('send then success result =', result);
       }).catch((error)=>{
         console.log('catch error =', error);
